f9f59617454fc282a305cd019fccd45654708c80
Sheedy21 <72637771+Sheedy21@users.noreply.github.com>
2020-12-30 15:07:58 +0000

    adding packages

--- Analysis_GIS.R
+++ Analysis_GIS.R
@@ -1,2 +1,22 @@
 
 # Packages
+
+library(tidyverse)
+library(tmap)
+library(geojsonio)
+library(plotly)
+library(rgdal)
+library(broom)
+library(mapview)
+library(crosstalk)
+library(sf)
+library(sp)
+library(spdep)
+library(car)
+library(fs)
+library(janitor)
+
+# Data
+
+download.file("https://data.london.gov.uk/download/statistical-gis-boundary-files-london/9ba8c833-6370-4b11-abdc-314aa020d5e0/statistical-gis-boundaries-london.zip", 
+                           destfile="practical_wk9/statistical-gis-boundaries-london")
